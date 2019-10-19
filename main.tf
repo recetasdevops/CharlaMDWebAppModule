5080d43d60927cb3ce47c5129e11cecb01ab36ab
recetasdevops <44634828+recetasdevops@users.noreply.github.com>
2019-10-19 13:57:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ locals {
   externalIp = split(",",azurerm_app_service.pasionporlosbits_webapp.outbound_ip_addresses)
 }
 
+data "azurerm_key_vault" "madriddotnet_key_vault" {
+  name                = "terraformMadridDotNet"
+  resource_group_name = "keyvault"
+}
+
 resource "azurerm_app_service_plan" "pasionporlosbits_serviceplan" {
   name                = "pasionporlosbits"
   location            = var.location
@@ -52,6 +57,22 @@ resource "azurerm_app_service_custom_hostname_binding" "pasionporlosbits_binding
   resource_group_name = var.resource_group
   
   depends_on = ["dnsimple_record.simpledns_pasionporlosbits_record","dnsimple_record.simpledns_pasionporlosbits_record_cname"]
+}
+
+#msi
+
+module "eg_key_vault_access_policies_fn_apps" {
+  source     = "git::https://github.com/recetasdevops/terraform-azurerm-app-service-key-vault-access-policy.git?ref=master"
+
+  access_policy_count = 1
+
+  identities              = azurerm_app_service.pasionporlosbits_webapp.identity
+  key_permissions         = ["get","list"]
+  secret_permissions      = ["get","list"]
+  certificate_permissions = ["get","list"]
+
+  key_vault_name   = data.azurerm_key_vault.madriddotnet_key_vault.name
+  key_vault_resource_group_name = data.azurerm_key_vault.madriddotnet_key_vault.resource_group_name
 }
 
 #certificate
